a5cadbf4b94756d56f1762d48d18f64754b80bac
aaronc1028 <81634761+aaronc1028@users.noreply.github.com>
2021-06-19 19:29:07 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("Welcome to VS Code!");
+console.log (15);
+var x=5;
+var y=50
+console.log(x);
+console.log(y)
+var firstName="Kyle"
+var lastName="Canepa"
+console.log (firstName + " " + lastName)
+var sentence="My Brothers Name is Kyle Canepa"
+console.log (sentence)
+var sentence3="My Name is Aaron Canepa"
+console.log (sentence3)
+var Firstname2="Lebron "
+var lastName2="James"
+console.log (Firstname2 + " " + lastName2)
+var sentence2="Lebron James is a Famous Basketball player."
+console.log (sentence2)
